5f06745519d26221c883b2c6dfcb846ce1c1b1a4
isieun0908 <supiya0908@naver.com>
2025-02-28 10:24:28 +0900

    Initial Spring Boot

---
+++ src/test/java/com/lssinni/AISM/AismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lssinni.AISM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
