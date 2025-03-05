827f855d8c379c2bcd5f927fdb5fb6efb13a46a3
isieun0908 <supiya0908@naver.com>
2025-03-05 13:58:50 +0900

    Feat: IamUser 객체 생성(#1)
    - Credential Report의 내용 저장할 객체 생성
    - Credential Report 항목에 맞는 멤버변수 선언

---
+++ src/main/java/com/lssinni/AISM/domain/IamUser.java
@@ -0,0 +1,50 @@
+package com.lssinni.AISM.domain;
+
+import java.time.LocalDateTime;
+
+public class IamUser {
+
+    private String user;        // 사용자의 표시 이름
+
+    private String arn;         // Amazon 리소스 이름
+
+    private LocalDateTime user_creation_time;       // 사용자가 생성된 날짜 및 시간
+
+    private Boolean password_enabled;               // 사용자의 암호 여부
+
+    private LocalDateTime password_last_used;       // 암호 마지막 사용한 날짜 및 시간
+
+    private LocalDateTime password_last_changed;    // 암호 마지막 설정된 날짜 및 시간
+
+    private LocalDateTime password_next_rotation;   // 새 암호 설정 날짜 및 시간
+
+    private Boolean mfa_active;             // MFA 사용 여부
+
+    private Boolean access_key_1_active;            // access_key1 활동 상태 여부
+
+    private LocalDateTime access_key_1_last_rotated;    // access_key1 마지막 변경일
+
+    private LocalDateTime access_key_1_last_used_date;  // access_key1 최근 사용일
+
+    private String access_key_1_last_used_region;       // access_key1 최근 사용 AWS Region
+
+    private String access_key_1_last_used_service;      // access_key1 최근 사용 AWS Service
+
+    private Boolean access_key_2_active;            // access_key2 활동 상태 여부
+
+    private LocalDateTime access_key_2_last_rotated;    // access_key2 마지막 변경일
+
+    private LocalDateTime access_key_2_last_used_date;  // access_key2 최근 사용일
+
+    private String access_key_2_last_used_region;       // access_key2 최근 사용 AWS Region
+
+    private String access_key_2_last_used_service;      // access_key2 최근 사용 AWS Service
+
+    private Boolean cert_1_active;          // 첫 번째 X.509 서명 인증서 상태 여부
+
+    private LocalDateTime cert_1_last_rotated;      // 첫 번째 서명인증서 마지막 변경 날짜 및 시간
+
+    private Boolean cert_2_active;          // 두 번째 X.509 서명 인증서 상태 여부
+
+    private LocalDateTime cert_2_last_rotated;      // 두 번째 서명인증서 마지막 변경 날짜 및 시간
+}
