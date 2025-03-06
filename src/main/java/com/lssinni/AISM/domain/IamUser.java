710dd476cbbe65aee60d298e358f23e77f49157d
isieun0908 <supiya0908@naver.com>
2025-03-06 18:01:26 +0900

    Refactor: IamUser 객체 멤버 변수 표기법 변경(Snake Case > Camel Case)

--- src/main/java/com/lssinni/AISM/domain/IamUser.java
+++ src/main/java/com/lssinni/AISM/domain/IamUser.java
@@ -13,43 +13,43 @@ public class IamUser {
 
     private String arn;         // Amazon 리소스 이름
 
-    private String user_creation_time;       // 사용자가 생성된 날짜 및 시간
+    private String userCreationTime;       // 사용자가 생성된 날짜 및 시간
 
-    private Boolean password_enabled;               // 사용자의 암호 여부
+    private Boolean passwordEnabled;               // 사용자의 암호 여부
 
-    private String password_last_used;       // 암호 마지막 사용한 날짜 및 시간
+    private String passwordLastUsed;       // 암호 마지막 사용한 날짜 및 시간
 
-    private String password_last_changed;    // 암호 마지막 설정된 날짜 및 시간
+    private String passwordLastChanged;    // 암호 마지막 설정된 날짜 및 시간
 
-    private String password_next_rotation;   // 새 암호 설정 날짜 및 시간
+    private String passwordNextRotation;   // 새 암호 설정 날짜 및 시간
 
-    private Boolean mfa_active;             // MFA 사용 여부
+    private Boolean mfaActive;             // MFA 사용 여부
 
-    private Boolean access_key_1_active;            // access_key1 활동 상태 여부
+    private Boolean accessKey1Active;            // access_key1 활동 상태 여부
 
-    private String access_key_1_last_rotated;    // access_key1 마지막 변경일
+    private String accessKey1LastRotated;    // access_key1 마지막 변경일
 
-    private String access_key_1_last_used_date;  // access_key1 최근 사용일
+    private String accessKey1LastUsedDate;  // access_key1 최근 사용일
 
-    private String access_key_1_last_used_region;       // access_key1 최근 사용 AWS Region
+    private String accessKey1LastUsedRegion;       // access_key1 최근 사용 AWS Region
 
-    private String access_key_1_last_used_service;      // access_key1 최근 사용 AWS Service
+    private String accessKey1LastUsedService;      // access_key1 최근 사용 AWS Service
 
-    private Boolean access_key_2_active;            // access_key2 활동 상태 여부
+    private Boolean accessKey2Active;            // access_key2 활동 상태 여부
 
-    private String access_key_2_last_rotated;    // access_key2 마지막 변경일
+    private String accessKey2LastRotated;    // access_key2 마지막 변경일
 
-    private String access_key_2_last_used_date;  // access_key2 최근 사용일
+    private String accessKey2LastUsedDate;  // access_key2 최근 사용일
 
-    private String access_key_2_last_used_region;       // access_key2 최근 사용 AWS Region
+    private String accessKey2LastUsedRegion;       // access_key2 최근 사용 AWS Region
 
-    private String access_key_2_last_used_service;      // access_key2 최근 사용 AWS Service
+    private String accessKey2LastUsedService;      // access_key2 최근 사용 AWS Service
 
-    private Boolean cert_1_active;          // 첫 번째 X.509 서명 인증서 상태 여부
+    private Boolean cert1Active;          // 첫 번째 X.509 서명 인증서 상태 여부
 
-    private String cert_1_last_rotated;      // 첫 번째 서명인증서 마지막 변경 날짜 및 시간
+    private String cert1LastRotated;      // 첫 번째 서명인증서 마지막 변경 날짜 및 시간
 
-    private Boolean cert_2_active;          // 두 번째 X.509 서명 인증서 상태 여부
+    private Boolean cert2Active;          // 두 번째 X.509 서명 인증서 상태 여부
 
-    private String cert_2_last_rotated;      // 두 번째 서명인증서 마지막 변경 날짜 및 시간
+    private String cert2LastRotated;      // 두 번째 서명인증서 마지막 변경 날짜 및 시간
 }
