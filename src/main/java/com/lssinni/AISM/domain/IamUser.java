6d295ebf675a670899d2066459518d06528a58c8
isieun0908 <supiya0908@naver.com>
2025-03-06 14:30:37 +0900

    Feat: IamUser 멤버변수 타입 LocalDateTime -> String 변경(#1)
    - LocalDateTime이었던 멤버변수 전부 String으로 타입 변경
    - 변경한 멤버변수: user_creation_time, password_last_used, password_last_changed,
      password_next_rotation, access_key_1_last_rotated,
      access_key_1_last_used_date, access_key_2_last_rotated,
      access_key_2_last_used_date, cert_1_last_rotated, cert_2_last_rotated
    - 변경 이유: 날짜값 이외에도 N/A, no_information, not_supported 등이
      담겨있어 각 값들을 저장한 후, 이후에 처리 예정

--- src/main/java/com/lssinni/AISM/domain/IamUser.java
+++ src/main/java/com/lssinni/AISM/domain/IamUser.java
@@ -1,30 +1,35 @@
 package com.lssinni.AISM.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.time.LocalDateTime;
 
+@Builder
+@Getter
 public class IamUser {
 
     private String user;        // 사용자의 표시 이름
 
     private String arn;         // Amazon 리소스 이름
 
-    private LocalDateTime user_creation_time;       // 사용자가 생성된 날짜 및 시간
+    private String user_creation_time;       // 사용자가 생성된 날짜 및 시간
 
     private Boolean password_enabled;               // 사용자의 암호 여부
 
-    private LocalDateTime password_last_used;       // 암호 마지막 사용한 날짜 및 시간
+    private String password_last_used;       // 암호 마지막 사용한 날짜 및 시간
 
-    private LocalDateTime password_last_changed;    // 암호 마지막 설정된 날짜 및 시간
+    private String password_last_changed;    // 암호 마지막 설정된 날짜 및 시간
 
-    private LocalDateTime password_next_rotation;   // 새 암호 설정 날짜 및 시간
+    private String password_next_rotation;   // 새 암호 설정 날짜 및 시간
 
     private Boolean mfa_active;             // MFA 사용 여부
 
     private Boolean access_key_1_active;            // access_key1 활동 상태 여부
 
-    private LocalDateTime access_key_1_last_rotated;    // access_key1 마지막 변경일
+    private String access_key_1_last_rotated;    // access_key1 마지막 변경일
 
-    private LocalDateTime access_key_1_last_used_date;  // access_key1 최근 사용일
+    private String access_key_1_last_used_date;  // access_key1 최근 사용일
 
     private String access_key_1_last_used_region;       // access_key1 최근 사용 AWS Region
 
@@ -32,9 +37,9 @@ public class IamUser {
 
     private Boolean access_key_2_active;            // access_key2 활동 상태 여부
 
-    private LocalDateTime access_key_2_last_rotated;    // access_key2 마지막 변경일
+    private String access_key_2_last_rotated;    // access_key2 마지막 변경일
 
-    private LocalDateTime access_key_2_last_used_date;  // access_key2 최근 사용일
+    private String access_key_2_last_used_date;  // access_key2 최근 사용일
 
     private String access_key_2_last_used_region;       // access_key2 최근 사용 AWS Region
 
@@ -42,9 +47,9 @@ public class IamUser {
 
     private Boolean cert_1_active;          // 첫 번째 X.509 서명 인증서 상태 여부
 
-    private LocalDateTime cert_1_last_rotated;      // 첫 번째 서명인증서 마지막 변경 날짜 및 시간
+    private String cert_1_last_rotated;      // 첫 번째 서명인증서 마지막 변경 날짜 및 시간
 
     private Boolean cert_2_active;          // 두 번째 X.509 서명 인증서 상태 여부
 
-    private LocalDateTime cert_2_last_rotated;      // 두 번째 서명인증서 마지막 변경 날짜 및 시간
+    private String cert_2_last_rotated;      // 두 번째 서명인증서 마지막 변경 날짜 및 시간
 }
